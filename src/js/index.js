82c70c3865e61861632b2ad3bb78548080f99154
reythedev <69159085+reythedev@users.noreply.github.com>
2020-08-19 18:42:29 +0400

    Add files

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+particlesJS.load(
+    'particles-js',
+    'https://raw.githubusercontent.com/reythedev/reythedev.github.io-source/master/particles.js-config.json',
+    function() {}
+);
+AOS.init();
